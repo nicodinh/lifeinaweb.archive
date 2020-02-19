946a4560bf8661a99cac06e0c4748acaee7aeb2b
nicolas <nicodinh@azari.fr>
2020-02-19 23:39:31 +0100

    Chuck Norris Emailed Me This Patch... I'm Not Going To Question It

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,10 @@
 import '../styles/index.css'
 
 export default () => (
-  <div className="p-4 shadow rounded bg-white">
-    <h1 className="text-purple-500 leading-normal">lifeinaweb</h1>
-    <p className="text-gray-500">with Tailwind CSS</p>
+  <div className='p-4 shadow rounded bg-white'>
+    <h1 className='text-purple-500 leading-normal'>lifeinaweb</h1>
+    <div className='p-4 shadow rounded bg-black'>
+      <p className='text-yellow-500'>with Tailwind CSS</p>
+    </div>
   </div>
 )
