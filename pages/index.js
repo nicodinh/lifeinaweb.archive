18fe50ad84aeb056b14a85d24bcad90db09a7fa1
nicolas <nicodinh@azari.fr>
2020-02-21 00:54:26 +0100

    :heart: Inside we both know what's been going on :heart_eyes:

--- pages/index.js
+++ pages/index.js
@@ -42,17 +42,17 @@ export default () => (
         <div className='h-auto p-4 shadow rounded bg-white text-center'>
           {/* content1 */}
           <div className='max-w-sm w-full lg:max-w-full lg:flex'>
-            <div
+            {/* <div
               className='h-48 lg:h-auto lg:w-48 flex-none bg-cover rounded-t lg:rounded-t-none lg:rounded-l text-center overflow-hidden'
               style={{ backgroundImage: 'url(' + '/lifeinabox.jpg' + ')' }}
               title='lifeinabox'
-            ></div>
+            ></div> */}
             <div className='border-r border-b border-l border-gray-400 lg:border-l-0 lg:border-t lg:border-gray-400 bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal'>
-              <div className='mb-8'>
+              {/* <div className='mb-8'>
                 <p className='text-sm text-gray-600 flex items-center'>
                   #1 LifeinaBox
                 </p>
-              </div>
+              </div> */}
               {/* <div className="flex items-center">
                 <div className="text-sm">
                   <p className="text-gray-900 leading-none">Jonathan Reinink</p>
