0ba80ab6aab11bb9cabe78efc1dd303d5dd1d749
nicolas <nicodinh@azari.fr>
2020-03-17 16:24:56 +0100

    :heart: Testing the test :heart_eyes:

--- components/SelectUnit.js
+++ components/SelectUnit.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { useStoreState, useStoreActions } from 'easy-peasy'
 
 const SelectUnit = () => {
-  const { units } = useStoreState(state => state.settings)
+  const { unit, units } = useStoreState(state => state.settings)
   const { updateUnit } = useStoreActions(actions => actions.settings)
 
   const handleChange = event => {
@@ -16,6 +16,7 @@ const SelectUnit = () => {
   return (
     <div className='inline-block relative w-64'>
       <select
+        defaultValue={unit}
         onChange={handleChange}
         className='block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline'
       >
