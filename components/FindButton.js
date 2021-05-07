20b68a4866bd990bc5141d703efdf4a2a7e59629
Nicolas Azari <nicodinh@azari.fr>
2021-05-07 17:28:12 +0200

    Update FindButton.js

--- components/FindButton.js
+++ components/FindButton.js
@@ -1,5 +1,3 @@
-/// <reference types="web-bluetooth" />
-
 import React from 'react'
 import { useStoreState, useStoreActions } from 'easy-peasy'
 import { lastBatteryBuffer, lastTemperatureBuffer } from '../lib/'
